56e9275471060fbd81ba6f291ca9836c9ffe5e1c
Abhiman67 <ironmanavengers6789@gmail.com>
2026-03-03 23:54:52 +0530

    Fix base path for Netlify + add netlify.toml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/RAPID_RASOI/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
